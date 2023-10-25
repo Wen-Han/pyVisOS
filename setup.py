5dc67c9aa73cba8f36da637c6be827c5c44a912f
Han Wen <hwen@lle.rochester.edu>
2023-10-25 18:12:57 -0400

    fix pip install

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 setup(name='pyVisOS',
       version='1.0',
       url='https://github.com/UCLA-Plasma-Simulation-Group/pyVisOS.git',
-      py_modules = ['osh5def','osh5gui','osh5io','osh5utils','osh5vis','osh5visipy']
+      py_modules = ['osh5def','osh5gui','osh5io','osh5utils','osh5vis','osh5visipy', 'osh5io_backend', 'osh5def_auxil']
       )
